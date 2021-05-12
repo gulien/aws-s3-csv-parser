c91a32a635a2c3f369ed12b5f5b2ea2518c9747c
Julien Neuhart <j.neuhart@thecodingmachine.com>
2021-05-12 22:28:31 +0200

    Add cmd logic

--- cmd/aws-s3-csv-parser/main.go
+++ cmd/aws-s3-csv-parser/main.go
@@ -1,12 +1,17 @@
+// aws-s3-csv-parser A simple CLI which downloads a CSV file from S3 and
+// inserts its data into a MySQL database.
 package main
 
 import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/aws-s3-csv-parser/pkg/storage"
+	"github.com/aws-s3-csv-parser/pkg/csvparser"
+	"github.com/aws-s3-csv-parser/pkg/datastorage"
+	"github.com/aws-s3-csv-parser/pkg/filestorage"
 	flag "github.com/spf13/pflag"
 )
 
@@ -17,7 +22,8 @@ func main() {
 	fs.String("region", "", "Set the AWS region")
 	fs.String("bucket", "", "Set the AWS S3 bucket")
 	fs.String("key", "", "Set the AWS S3 object key to download")
-	fs.Int("timeout", 300, "Set the maximum duration in seconds before timing out the download")
+	fs.Bool("skip-download", false, "Skip download if true (i.e, file has already been downloaded)")
+	fs.Int("timeout", 300, "Set the maximum duration in seconds before timing out")
 
 	// Parses the flags...
 	err := fs.Parse(os.Args[1:])
@@ -47,24 +53,88 @@ func main() {
 		os.Exit(1)
 	}
 
+	skipDownload, err := fs.GetBool("skip-download")
+	if err != nil {
+		fmt.Printf("[FATAL] %s\n", err)
+		os.Exit(1)
+	}
+
 	timeout, err := fs.GetInt("timeout")
 	if err != nil {
 		fmt.Printf("[FATAL] %s\n", err)
 		os.Exit(1)
 	}
 
-	w, err := os.Create("tmp.csv")
+	timeoutDuration := time.Duration(timeout) * time.Second
+
+	inserter, err := datastorage.NewMySQLInserter(
+		os.Getenv("MYSQL_DATABASE_URL"),
+		"events",
+		[]string{
+			"id",
+			"timestamp",
+			"email",
+			"country",
+			"ip",
+			"uri",
+			"action",
+			"tags",
+		},
+		timeoutDuration,
+	)
+
 	if err != nil {
 		fmt.Printf("[FATAL] %s\n", err)
 		os.Exit(1)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
 	defer cancel()
 
-	downloader := storage.NewS3PublicDownloader(region, bucket, key)
+	const tmpFilename = "tmp.csv"
 
-	err = downloader.Download(ctx, w)
+	if !skipDownload {
+		w, err := os.Create(tmpFilename)
+		if err != nil {
+			fmt.Printf("[FATAL] %s\n", err)
+			os.Exit(1)
+		}
+
+		downloader := filestorage.NewS3PublicDownloader(region, bucket, key)
+
+		fmt.Println("[INFO] Downloading file...")
+
+		err = downloader.Download(ctx, w)
+		if err != nil {
+			fmt.Printf("[FATAL] %s\n", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("[INFO] Parsing CSV...")
+
+	err = csvparser.Parse(
+		ctx,
+		tmpFilename,
+		csvparser.Callback{
+			Every: 1000,
+			Do: func(ctx context.Context, records [][]string) error {
+				fmt.Printf("[INFO] Processing %d records...\n", len(records))
+
+				return inserter.Insert(ctx, records, datastorage.InsertOptions{
+					Formatter: func(value string) string {
+						return strings.ReplaceAll(value, "'", "\"")
+					},
+					IgnoreDuplicateError: true,
+				})
+			},
+		},
+		csvparser.Options{
+			Comma:         ',',
+			SkipFirstLine: true,
+		},
+	)
+
 	if err != nil {
 		fmt.Printf("[FATAL] %s\n", err)
 		os.Exit(1)
